a079451c317bd7662535282e29ac6c7107b5a999
Ubermensh-Skull-Eyes <nickygamers606@gmail.com>
2026-08-26 15:59:06 +0530

    Added UI for rotor and reflector

--- reflector.py
+++ reflector.py
@@ -1,3 +1,4 @@
+import pygame
 class Reflector:
     def __init__(self,wiring,notch):
         self.left = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -7,3 +8,16 @@ class Reflector:
         letter = self.right[signal]
         signal = self.left.find(letter)
         return signal
+    def draw(self,screen ,x,y,w,h,font):
+                    r = pygame.Rect(x,y,w,h)
+                    pygame.draw.rect(screen,'black',r,width=2,border_radius=2)
+                    for i in range(26):
+                        letter = self.right
+                        letter = font.render(letter[i],False,'black')
+                        text_box = letter.get_rect(center = (x+3*w/4,y+(i+1)*h/27))
+                        screen.blit(letter,text_box)
+                    for i in range(26):
+                        letter = self.left
+                        letter = font.render(letter[i],False,'black')
+                        text_box = letter.get_rect(center = (x+w/4,y+(i+1)*h/27))
+                        screen.blit(letter,text_box)
--- rotor.py
+++ rotor.py
@@ -1,3 +1,4 @@
+import pygame
 class Rotor:
     def __init__(self,wiring,notch):
         self.left = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -32,6 +33,26 @@ class Rotor:
          self.rotate(n)
 
     def ring(self,n):
+        # n = self.right.find(letters)
         self.rotate(n-1,False)
         n_notch = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".find(self.notch)
         self.notch = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[(n_notch-n)%26]
+
+    def draw(self,screen ,x,y,w,h,font):
+                r = pygame.Rect(x,y,w,h)
+                pygame.draw.rect(screen,'black',r,width=2,border_radius=2)
+                for i in range(26):
+                    letter = self.right
+                    letter = font.render(letter[i],False,'black')
+                    text_box = letter.get_rect(center = (x+3*w/4,y+(i+1)*h/27))
+                    screen.blit(letter,text_box)
+                for i in range(26):
+                    letter = self.left
+                    letter = font.render(letter[i],False,'black')
+                    text_box = letter.get_rect(center = (x+w/4,y+(i+1)*h/27))
+                    if i==0:
+                        pygame.draw.rect(screen,'cyan',text_box,border_radius=5)
+                    if self.left[i]==self.notch:
+                         letter = font.render(self.notch,False,'white')
+                         pygame.draw.rect(screen,'#333333',text_box,border_radius=5)
+                    screen.blit(letter,text_box)
